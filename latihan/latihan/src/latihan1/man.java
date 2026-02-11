42e3df54156faeb8e64bc4f2b979831f91820e18
mata-riz <abiilalcholisyi@gmail.com>
2026-02-11 11:35:10 +0700

    praktikum1

---
+++ latihan/latihan/src/latihan1/man.java
@@ -0,0 +1,23 @@
+package latihan1;
+
+public class man {
+    class Cafe(
+        String nama;
+        String alamat;
+
+        // contructor
+        Cafe(String nama, String alamat){
+            this.nama = nama;
+            this.alamat = alamat;
+            System.out.printl("nama cafenya adalah : + nama");
+            System.out.printl("alamat nya di : + alamat");
+            System.out.printl(x: "===============");
+        }
+    )
+}
+
+public class main {
+    public static void main(String[] args) {
+        Cafe cafe1 =new Cafe (nama: "kenagan", alamat: "IMKOT")
+    }
+}
